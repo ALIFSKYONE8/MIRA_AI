6660dcb977ec14e19c188aa16319b49d7bdb95e2
SKYONE <alifskyone8@gmail.com>
2025-12-07 19:39:23 +0800

    Commit pertama: menambah fail HTML, CSS, dan JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Dapatkan elemen butang dari HTML
+const myButton = document.getElementById('myButton');
+
+// Tambah fungsi apabila butang diklik
+myButton.addEventListener('click', function() {
+    alert('Tahniah! JavaScript anda berfungsi.');
+});
